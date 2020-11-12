2d0d50a4c44131ad6935ef21e8a331cdafc74875
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-12 08:25:53 -0800

    Created database tables for Messages and Comments(the new models are inside of app_wall)

--- The_Wall/app_wall/models.py
+++ The_Wall/app_wall/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 from app_LR.models import User
 # Create your models here.
+
+class Messages(models.Model):
+    user = User()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comments(models.Model):
+    message = Messages()
+    user = User()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
