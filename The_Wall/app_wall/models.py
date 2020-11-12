67c4493bce2c8948037b80c648d9e1856871dca7
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-12 08:08:37 -0800

    Setup routes to test and imported app_LR models to app_wall

--- The_Wall/app_wall/models.py
+++ The_Wall/app_wall/models.py
@@ -1,3 +1,3 @@
 from django.db import models
-
+from app_LR.models import User
 # Create your models here.
--- The_Wall/app_wall/views.py
+++ The_Wall/app_wall/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, HttpResponse
+from  .models import *
 # Create your views here.
+
+def wall(request):
+    return HttpResponse('Hi Human')
+    # return render(request, 'wall.html')
